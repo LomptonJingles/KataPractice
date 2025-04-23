3433316d9f3d81296f3c42d922f6f4fc9f799d97
guydu <thecoolonesaidmyname@gmail.com>
2025-04-23 15:46:25 -0400

    tests repository area

---
+++ src/test/java/RomanNumeralTest.java
@@ -0,0 +1,12 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class RomanNumeralTest {
+
+    @Test
+    public void helloWorldTest() {
+        String hello = "Hello World!";
+
+        Assertions.assertEquals(hello, "Hello World!");
+    }
+}
