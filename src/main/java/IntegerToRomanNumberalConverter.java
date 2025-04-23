7703c3cbdb3580be232b7a54b911c92848e4376c
guydu <thecoolonesaidmyname@gmail.com>
2025-04-23 16:05:44 -0400

    adds minimum code for test to work. Green Step.

---
+++ src/main/java/IntegerToRomanNumberalConverter.java
@@ -0,0 +1,8 @@
+public class IntegerToRomanNumberalConverter {
+    public String convertToRomanNumeral(int number) {
+        if (number == 1) {
+            return "I";
+        }
+        return "ERROR";
+    }
+}
