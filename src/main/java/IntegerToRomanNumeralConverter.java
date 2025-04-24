6f13b90add338e89b3797df3e844e543a4e90f04
guydu <thecoolonesaidmyname@gmail.com>
2025-04-24 11:59:31 -0400

    makes the code run by replacing if with while loops red step.

--- src/main/java/IntegerToRomanNumeralConverter.java
+++ src/main/java/IntegerToRomanNumeralConverter.java
@@ -2,20 +2,20 @@ public class IntegerToRomanNumeralConverter {
     public String convertToRomanNumeral(int number) {
         StringBuilder romanNumeral = new StringBuilder();
         while (number > 0) {
-            if(number >= 9) {
+            while (number >= 9) {
                 romanNumeral.append("X");
                 number -= 10;
 
                 if (number < 0) {
-                    romanNumeral.insert(-1, "I");
+                    romanNumeral.insert(0, "I");
                 }
             }
-            if (number >= 4) {
+            while (number >= 4) {
                 romanNumeral.append("V");
                 number -= 5;
 
                 if (number < 0) {
-                    romanNumeral.insert(-1, "I");
+                    romanNumeral.insert(0, "I");
                 }
             }
             if (number >= 1) {
