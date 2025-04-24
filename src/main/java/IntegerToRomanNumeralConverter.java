8700ddc52ea0ce46f5b9923b16c3a1580a550ecd
guydu <thecoolonesaidmyname@gmail.com>
2025-04-24 11:42:09 -0400

    Changed the code until it worked. Refactored code and removed code that no longer worked. Green step.

--- src/main/java/IntegerToRomanNumeralConverter.java
+++ src/main/java/IntegerToRomanNumeralConverter.java
@@ -1,23 +1,28 @@
 public class IntegerToRomanNumeralConverter {
     public String convertToRomanNumeral(int number) {
         StringBuilder romanNumeral = new StringBuilder();
-        int repeatingI = 0;
         while (number > 0) {
-            if (number >= 5) {
+            if (number >= 9) {
+                romanNumeral.append("X");
+                number -= 10;
+
+                if (number < 0) {
+                    romanNumeral.insert(0, "I");
+                }
+            }
+            if (number >= 4) {
                 romanNumeral.append("V");
                 number -= 5;
+
+                if (number < 0) {
+                    romanNumeral.insert(0, "I");
+                }
             }
             if (number >= 1) {
                 romanNumeral.append("I");
                 number -= 1;
-                repeatingI++;
-            }
-            if (repeatingI > 3) {
-                romanNumeral.insert(1,"V");
-                romanNumeral.delete(2,5);
             }
         }
-
         return romanNumeral.toString();
     }
 }
